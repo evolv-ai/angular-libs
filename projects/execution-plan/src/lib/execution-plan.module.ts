bbe3c596e1b97b2f03a1de954c81358225db29e5
Rob Severn <robert.severn@evolv.ai>
2020-03-09 10:17:52 -0700

    Adding the Execution Plan module to the library

---
+++ projects/execution-plan/src/lib/execution-plan.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import {EvolvDirective} from "./directives/evolv.directive";
+
+
+
+@NgModule({
+  declarations: [
+    EvolvDirective
+  ],
+  imports: [
+  ],
+  exports: [
+    EvolvDirective
+  ]
+})
+export class ExecutionPlanModule { }
