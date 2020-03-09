48e14b9c9908be35998931e25cc502709cc721bb
Rob Severn <robert.severn@evolv.ai>
2020-03-08 17:27:54 -0700

    Initial lib commit for the evolv resolver

---
+++ projects/execution-plan/src/lib/services/evolv.resolver.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EvolvResolver implements Resolve<any> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<any> {
+    const evolv = (window as any).evolv;
+    return (!evolv)
+      ? Promise.reject()
+      : evolv.runtime;
+  }
+}
+
+
