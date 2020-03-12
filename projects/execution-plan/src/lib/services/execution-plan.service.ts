49143c1495931cb599a25582b89f0702c1ec19bc
Rob Severn <robert.severn@evolv.ai>
2020-03-12 08:54:22 -0700

    Lint errors

--- projects/execution-plan/src/lib/services/execution-plan.service.ts
+++ projects/execution-plan/src/lib/services/execution-plan.service.ts
@@ -1,5 +1,5 @@
 import {Inject, Injectable, InjectionToken, Optional} from '@angular/core';
-import {timer} from "rxjs";
+import {timer} from 'rxjs';
 
 export const EXECUTION_PLAN_MAX_WAIT = new InjectionToken<string>('executionPlanMaxWait');
 
@@ -16,16 +16,16 @@ export class ExecutionPlanService {
   }
 
   public getEvolv(): Promise<any> {
-    let startTime = Date.now();
+    const startTime = Date.now();
     return new Promise((resolve, reject) => {
-      let subscription = timer(0, 10).subscribe((t) => {
-        let evolv = this._extractEvolvFromWindow();
+      const subscription = timer(0, 10).subscribe((t) => {
+        const evolv = this._extractEvolvFromWindow();
         if (evolv) {
           subscription.unsubscribe();
           resolve(evolv);
         } else if (Date.now() - startTime > this.maxWait) {
           subscription.unsubscribe();
-          reject(`Evolv not found within ${this.maxWait}ms`)
+          reject(`Evolv not found within ${this.maxWait}ms`);
         }
       });
     });
@@ -39,7 +39,7 @@ export class ExecutionPlanService {
         })
         .catch((err) => {
           reject(err);
-        })
+        });
     });
   }
 }
